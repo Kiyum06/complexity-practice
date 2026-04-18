f5af9fd4afaf97316fb7fdcd3d0629dd44be50d4
Your Name <kiyum1406@gmail.com>
2026-04-18 00:30:51 -0700

    answered emailLookup(), emailLookupEfficient() complexity

--- src/Main.java
+++ src/Main.java
@@ -158,7 +158,7 @@ public class Main {
   // Otherwise, it returns "Person not found"
   // assume that each String is bounded by a constant length
   // What is the time complexity of this method?
-  // YOUR ANSWER HERE
+  // O(n)
   public static String emailLookup(String[] names, String[] emails, String queryName) {
     for(int i = 0; i < names.length; i++) {
       if (names[i].equals(queryName)) {
@@ -174,7 +174,7 @@ public class Main {
   // Write this method to efficiently return the corresponding email or "Person not found" if appropriate
   // assume that each String is bounded by a constant length
   // What is the time complexity of your solution?
-  // YOUR ANSWER HERE
+  // O(1)
   public static String emailLookupEfficient(HashMap<String, String> namesToEmails, String queryName) {
     return null;
   }
